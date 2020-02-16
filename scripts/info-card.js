4c65d7c841ee91c204eaae95c13da23af70d795c
Timothy Williams-Fowler <timwfowler0@gmail.com>
2020-02-16 17:03:04 +0700

    Update info-card.js

--- scripts/info-card.js
+++ scripts/info-card.js
@@ -9,22 +9,17 @@ for (var i = 0; i < x.length; i++) {
   let counter = i
   x[i].addEventListener("mouseover", function(){
     console.log(x[counter].childNodes[1]);
-    x[counter].classList.add("active-color");
-    x[counter].childNodes[1].classList.add("active");
-    x[counter].childNodes[3].classList.add("active-header");
-    x[counter].childNodes[5].classList.add("active");
-    x[counter].childNodes[7].classList.add("active-show");
-    console.log("hi" + x[counter].childNodes[7]);
+    x[counter].classList.add("active-card");
+    // x[counter].childNodes[1].classList.add("active");
+    // x[counter].childNodes[3].classList.add("active-header");
+    // x[counter].childNodes[5].classList.add("active");
+    // x[counter].childNodes[7].classList.add("active-show");
+    // console.log("hi" + x[counter].childNodes[7]);
     //x[counter].childNodes[4].classList.add("active");
 
   })
   x[i].addEventListener("mouseleave", function(){
-    console.log(x[counter].childNodes[1]);
-    x[counter].classList.remove("active-color");
-    x[counter].childNodes[1].classList.remove("active");
-    x[counter].childNodes[3].classList.remove("active-header");
-    x[counter].childNodes[5].classList.remove("active");
-    x[counter].childNodes[7].classList.remove("active-show");
+x[counter].classList.remove("active-card");
   })
   ;
 }
